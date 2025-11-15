1c3c604999de99f8d1b01b45a85e48fd142e71be
sudipsamanta777 <imsudip789@gmail.com>
2025-11-15 20:33:51 +0530

    Adding a c file to calculate the sum of two integer numbers

---
+++ Sudip2.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+    int n1, n2, sum;
+    printf("Enter number1 : \n");
+    scanf("%d", &n1);
+    printf("Enter number2 : \n");
+    scanf("%d", &n2);
+    sum = n1 + n2;
+    printf("Sum of number1 and number2 = %d", sum);
+    return 0;
+}
